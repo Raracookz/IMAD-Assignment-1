2e5c46b4f53aad6f9a8809b5f472d67a6ea61294
Raracookz <162429322+Raracookz@users.noreply.github.com>
2024-05-06 10:43:18 +0200

    first commit

---
+++ statscalculator/app/src/main/java/com/example/statscalculator/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.statscalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val = findViewById<Button>(R.id.btn_subtract)
+
+
+
+
+
+
+        
+    }
+}
